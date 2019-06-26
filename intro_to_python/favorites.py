aefc07cb1ee36696f98af9f33ed9bb1d55dbb5bd
Josh Archer <jarcher@greenriver.edu>
2019-06-26 09:08:08 -0700

    Renamed our variables to have better names!

--- intro_to_python/favorites.py
+++ intro_to_python/favorites.py
@@ -4,20 +4,20 @@
 # File: favorites.py
 
 # these are our three favorite shows
-show1 = "House"
-show2 = "Criminal Minds"
-show3 = "Power"
+house = "House"
+criminalMinds = "Criminal Minds"
+power = "Power"
 
 print("My favorite shows: ")
-print(show1)
-print(show2)
-print(show3)
+print(house)
+print(criminalMinds)
+print(power)
 print() # new line...
 
 # these are our two favorite books
-book1 = "The Fountain Head"
-book2 = "Teeth of the Tiger"
+fountainHead = "The Fountain Head"
+teethOfTiger = "Teeth of the Tiger"
 
 print("My favorite books: ")
-print(book1)
-print(book2)
+print(fountainHead)
+print(teethOfTiger)
