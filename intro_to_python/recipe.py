8029c6371309836c8ab7c5b9c6157742cd26cde4
Josh Archer <jarcher@greenriver.edu>
2019-07-01 08:11:13 -0700

    Added the recipe example from our slides.

---
+++ intro_to_python/recipe.py
@@ -0,0 +1,41 @@
+# This is a recipe for my favorite food!
+# Author: Josh Archer
+# Date: 6/22/2019
+# File: recipe.py
+
+flour = "2 3/4 cups all-purpose flour"
+bakingSoda = "1 teaspoon baking soda"
+bakingPowder = "1/2 teaspoon baking powder"
+butter = "1 cup butter, softened"
+sugar = "1 1/2 cups white sugar"
+egg = "1 egg"
+vanilla = "1 teaspoon vanilla extract"
+
+#ingredients
+print("Ingredients **************")
+print(flour)
+print(bakingSoda)
+print(bakingPowder)
+print(butter)
+print(sugar)
+print(egg)
+print(vanilla)
+print() #new line
+
+#steps
+print("Steps ********************")
+print("1. Preheat oven to 375 degrees F (190 degrees C). ")
+print("In a small bowl, stir together flour, baking soda, ")
+print("and baking powder. Set aside.")
+print() #new line
+
+print("2. In a large bowl, cream together the butter and ")
+print("sugar until smooth. Beat in egg and vanilla. ")
+print("Gradually blend in the dry ingredients. Roll rounded ")
+print("teaspoonfuls of dough into balls, and place onto ")
+print("ungreased cookie sheets. ")
+print() #new line
+
+print("3. Bake 8 to 10 minutes in the preheated oven, or until ")
+print("golden. Let stand on cookie sheet two minutes before ")
+print("removing to cool on wire racks. ")
